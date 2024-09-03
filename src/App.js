9f5330d11b1be6380c7a8d6e7bfa711a519734c6
Micha <michael.rigas@t-online.de>
2024-09-03 10:13:32 +0200

    create frontend

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
-import React, { useState, useEffect } from "react"; // React Hooks importieren
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavigationBar from "./components/layout/navbar";
 import "./styles/App.css";
+import "./styles/GlobalVars.css";
+import HomePage from "./pages/home-page";
+import ImpressumPage from "./pages/impressum-page";
+import Content from "./components/layout/content";
+import AboutMePage from "./pages/aboutme-page";
+import Footer from "./components/layout/footer";
+import ToDoPage from "./pages/todo-page/";
 
 function App() {
-  // useState Hook zum Verwalten eines Zählers
-  const [count, setCount] = useState(0);
-
-  // useEffect Hook, der nach dem ersten Rendern ausgeführt wird
-  useEffect(() => {
-    console.log("Die App-Komponente wurde geladen!");
-
-    // Cleanup-Funktion (optional), die ausgeführt wird, wenn die Komponente unmounted wird
-    return () => {
-      console.log("Die App-Komponente wird entladen.");
-    };
-  }, []); // Leeres Array als Abhängigkeit -> useEffect wird nur einmal beim Mount ausgeführt
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        {/* Zähler Sektion mit zwei Buttons */}
-        <div className="counter-section">
-          <h1>Count: {count}</h1>
-          <button onClick={() => setCount(count + 1)}>increase count</button>
-          <button onClick={() => setCount(count - 1)}>decrease count</button>
-        </div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavigationBar />
+      <Content>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/impressum" element={<ImpressumPage />} />
+          <Route path="/aboutme" element={<AboutMePage />} />
+          <Route path="/todo" element={<ToDoPage />} />
+        </Routes>
+      </Content>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
